224824684438e45a6a90392d91e4b7a7303ae3c4
bharathikannan <i.bharathikannan@gmial.com>
2025-01-05 21:34:52 +0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Bharathikannan Ilanchezhiyan',
+    author_email= 'i.bharathikannan@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
